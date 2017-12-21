64d89654934800db97e3764ba1a00fc13cf28bdc
Samuel Colvin <s@muelcolvin.com>
2017-12-21 17:21:34 +0000

    ttl for events

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+#!/home/samuel/code/mouse-events/env/bin/python
 import re
 import struct
 import sys
@@ -39,6 +40,7 @@ CODES = {
     # (1, 280, 0): zoom,
     (1, 280, 1): zoom,
 }
+REPEAT_TTL = 0.6
 
 
 def get_devices():
@@ -51,22 +53,36 @@ def get_devices():
 
 def main():
     devs = get_devices()
-    mouse_name = 'Logitech Performance MX'
-    dev = devs.get(mouse_name)
+    mouse_names = 'Logitech Performance MX', 'Logitech Unifying Device'
+    dev = None
+    for name in mouse_names:
+        dev = next((v for k, v in devs.items() if name in k), None)
+        if dev:
+            break
     if not dev:
-        print(f'device "{mouse_name}" not found')
+        devs = '\n  '.join(devs)
+        print(f'no devices found: "{mouse_names}", devices:\n{devs}')
         sys.exit(1)
     try:
+        last_event = None
+        last_event_time = 0
         with open(f'/dev/input/event{dev}', 'rb') as f:
             while True:
                 event = f.read(EVENT_SIZE)
                 if event:
                     _, _, type_, code, value = struct.unpack(FORMAT, event)
-                    event = CODES.get((type_, code, value))
-                    event and event()
-                    # event and print(event)
                     # if (type_, code, value) != (0, 0, 0):
                     #     print(f'type: {type_} code: {code} value: {value}')
+                    event = CODES.get((type_, code, value))
+                    if event:
+                        now = time.time()
+                        e_name = event.__name__
+                        run = last_event != e_name or now > (last_event_time + REPEAT_TTL)
+                        print(e_name, f'{now - last_event_time:0.2f}s', 'run' if run else '-')
+                        if run:
+                            event()
+                            last_event = e_name
+                            last_event_time = now
                 else:
                     time.sleep(0.01)
     except KeyboardInterrupt:
