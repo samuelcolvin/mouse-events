8f3382119bc9f849efea2b5e570854876152ec60
Samuel Colvin <s@muelcolvin.com>
2018-03-17 13:08:33 +0000

    verbose mode and better input names

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 #!/home/samuel/code/mouse-events/env/bin/python
-import re
 import struct
 import sys
 import time
+from pathlib import Path
 
+import fcntl
 from pyautogui import keyDown, keyUp, press
+from ioctl_opt import IOC, IOC_READ
 
 FORMAT = 'llHHI'
 EVENT_SIZE = struct.calcsize(FORMAT)
@@ -33,7 +35,6 @@ def zoom():
     keyUp('winleft')
 
 
-
 CODES = {
     (2, 6, 2**32 - 1): left,
     (2, 6, 1): right,
@@ -43,36 +44,50 @@ CODES = {
 REPEAT_TTL = 0.6
 
 
+def EVIOCGNAME(length):
+    return IOC(IOC_READ, ord('E'), 0x06, length)
+
+
 def get_devices():
-    with open('/proc/bus/input/devices') as f:
-        text = f.read()
+    paths = sorted([f for f in Path('/dev/input').iterdir() if f.name.startswith('event')],
+                   key=lambda f: int(f.name.replace('event', '')))
+    devices = []
+    for f in paths:
+        buffer = b'\0' * 512
+        with f.open() as fd:
+            name = fcntl.ioctl(fd, EVIOCGNAME(512), buffer)
+            name = name[:name.find(b'\0')].decode()
+        devices.append((str(f), name))
+    return devices
 
-    matches = re.findall(r'N: Name="(.+?)"\nP: .*\nS: .*input(\d+)', text)
-    return dict(matches)
 
+def main(verbose_):
+    def verbose(*args, **kwargs):
+        verbose_ and print(*args, **kwargs)
 
-def main():
     devs = get_devices()
     mouse_names = 'Logitech Performance MX', 'Logitech Unifying Device'
     dev = None
-    for name in mouse_names:
-        dev = next((v for k, v in devs.items() if name in k), None)
+    devs_str = '\n'.join(f'  {file:>20}: {name}' for file, name in devs)
+    verbose('devices:\n' + devs_str)
+    for match in mouse_names:
+        dev, dev_name = next(([file, name] for file, name in devs if match in name), (None, None))
         if dev:
+            print(f'using: {dev} "{dev_name}"')
             break
     if not dev:
-        devs = '\n  '.join(devs)
-        print(f'no devices found: "{mouse_names}", devices:\n{devs}')
+        print(f'no devices found: "{mouse_names}", devices:\n{devs_str}')
         sys.exit(1)
     try:
         last_event = None
         last_event_time = 0
-        with open(f'/dev/input/event{dev}', 'rb') as f:
+        with open(dev, 'rb') as f:
             while True:
                 event = f.read(EVENT_SIZE)
                 if event:
                     _, _, type_, code, value = struct.unpack(FORMAT, event)
-                    # if (type_, code, value) != (0, 0, 0):
-                    #     print(f'type: {type_} code: {code} value: {value}')
+                    if verbose_ and (type_, code, value) != (0, 0, 0):
+                        print(f'type: {type_} code: {code} value: {value}')
                     event = CODES.get((type_, code, value))
                     if event:
                         now = time.time()
@@ -90,4 +105,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main('--verbose' in sys.argv or '-v' in sys.argv)
